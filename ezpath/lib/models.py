9689cf84a7b94ed94b44ca2c8c6bdc3ffbd642c3
Yihuan Song <ysong@ysong-mbp.local>
2014-04-20 13:13:27 -0700

    remove ignored

--- ezpath/lib/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class CommonBaseModel(models.Model):
-    datetime_created = models.DateTimeField(auto_now_add=True)
-    datetime_modified = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        abstract = True
